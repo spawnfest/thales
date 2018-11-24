21d8fabb97741bf1006db99adf4fce96e60d26c5
Ilhan Adiyaman <ilhanadiyamann@gmail.com>
2018-11-24 14:37:44 +0100

    variable added

--- src/thales.erl
+++ src/thales.erl
@@ -4,8 +4,16 @@
 %%% Created :  24 Nov 2018 by Ilhan Adiyaman <ilhanadiyaman@yahoo.com>
 
 -module(thales).
+-export([variable/1, test/2]).
 
--export([info/1]).
+-include("node.hrl").
 
-info(A) ->
-  {ok, A}.
+%% User defined variables in an expression.
+variable(Name) ->
+  PlaceholderNode = placeholder_op:op(),
+  PlaceholderNode0 = PlaceholderNode#node{name = Name},
+  PlaceholderNode0.
+
+test(Var1, Var2) ->
+  Y = node:add(Var1, Var2),
+  Y.
